46e88a5c689406a6eb450932390882b7dada2a77
t112310218 <146065077+t112310218@users.noreply.github.com>
2023-10-31 14:22:41 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int h,m;
+    double output;
+
+    scanf("%d%d", &h, &m);
+
+    if(h > 120){
+        output += (h - 120) * 1.66 * m;
+        h = 120;
+    }
+    if(h > 60){
+        output += (h - 60) * 1.33 * m;
+        h = 60;
+    }
+    output += h * m;
+    printf("%.1f", output);
+    return 0;
+}
 
