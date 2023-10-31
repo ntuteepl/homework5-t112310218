5855fe346de4256e7a204313a0730012f400116e
t112310218 <146065077+t112310218@users.noreply.github.com>
2023-10-31 14:22:28 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int input;
+    double output;
+
+    scanf("%d", &input);
+
+    output = input * 0.9;
+
+    if(input >= 1500){
+        output *= 0.75;
+    }else if(input >= 800){
+        output *= 0.9;
+    }
+
+    printf("%.1f", output);
+    return 0;
+}
 
