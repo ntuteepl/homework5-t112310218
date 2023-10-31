4513ca2bfa9531fb92e3ff147cc57a7eaa922ca7
t112310218 <146065077+t112310218@users.noreply.github.com>
2023-10-31 14:22:56 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int h, m;
+    double hAngle, mAngle, output;
+    scanf("%d%d", &h, &m);
+
+    hAngle = h * 30.0 + (30.0 / 60.0 * m);
+
+    mAngle = 6.0 * m;
+
+    if(hAngle >= mAngle){
+        output = hAngle - mAngle;
+    }else{
+        output = mAngle - hAngle;
+    }
+
+    if(output > 180) output = 360 - output;
+
+    printf("%.3f", output);
+
+    return 0;
+}
 
